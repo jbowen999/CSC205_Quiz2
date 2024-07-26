5d4d54e514ad5bd74034fc82f32aff07bc314b93
jbowen999 <julian.e.bowen@outlook.com>
2024-07-26 16:28:55 -0700

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC205_Quiz2
+{
+    internal class Car
+    {
+        private string _vin;
+        private string _make;
+        private string _model;
+        private int _mileage;
+
+        //public int Mileage { get { return _mileage; } }
+
+        public Car(string vin, string make, string model)
+        {
+            _vin = vin;
+            _make = make;
+            _model = model;
+        }
+        public void Drive(int miles)
+        {
+        _mileage += miles; 
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Make: {_make}");
+            Console.WriteLine($"Model: {_model}");
+            Console.WriteLine($"Mileage: {_mileage}");
+            Console.WriteLine($"Vin: {_vin}");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CSC205_Quiz2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car toyota = new Car("12345678909876543", "Toyota", "Corolla");
+            Car kia = new Car("09876543212345678", "Kia", "Optima");
+            toyota.Drive(150000);
+            toyota.Drive(3500);
+            kia.Drive(42000);
+            toyota.Display();
+            Console.WriteLine("----------------------------------");
+            kia.Display();
+        }
+    }
+}
